2be40151220b83e651a7c666ee180abb55e2ee7e
Takao Kitano <tkitano@gmail.com>
2014-05-18 07:19:03 +0000

    added reverse proxy support

--- unicorn.rb
+++ unicorn.rb
@@ -1,12 +1,12 @@
-@dir = File.dirname(__FILE__)
+@dir = File.expand_path(File.dirname(__FILE__))
 
 worker_processes 1
 working_directory @dir
 
 timeout 30
 
-#listen "#{@dir}/server/unicorn.sock", :backlog => 64
-listen 9292
+listen "#{@dir}/server/unicorn.sock", :backlog => 64
+#listen 9292
 
 pid "#{@dir}/server/unicorn.pid"
 stdout_path "#{@dir}/server/unicorn.stdout.log"
