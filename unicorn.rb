306fc54ad664edf85445eb5069cfb3387a135015
Takao Kitano <tkitano@gmail.com>
2014-07-02 17:06:51 +0900

    changed to listen port 9292 (instead of listening on a unix domain socket)

--- unicorn.rb
+++ unicorn.rb
@@ -1,12 +1,12 @@
 @dir = File.expand_path(File.dirname(__FILE__))
 
-worker_processes 1
+worker_processes 4
 working_directory @dir
 
 timeout 30
 
-listen "#{@dir}/server/unicorn.sock", :backlog => 64
-#listen 9292
+#listen "#{@dir}/server/unicorn.sock", :backlog => 64
+listen 9292
 
 pid "#{@dir}/server/unicorn.pid"
 stdout_path "#{@dir}/server/unicorn.stdout.log"
