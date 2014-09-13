153df098a301407aca99d78fac20ad985533dc18
Takao Kitano <tkitano@gmail.com>
2014-09-13 07:33:59 +0000

    added test/production checking

--- carnation_deploy.sh
+++ carnation_deploy.sh
@@ -11,6 +11,16 @@ env_name=$1
 address=$2
 echo "env_name:${env_name}"
 echo "server address:${address}"
+if [[ $env_name = 'production' ]]
+then
+  echo "config carnation server as production server"
+elif [[ $env_name = 'test' ]]
+then
+  echo "config carnation server as test server"
+else
+  echo "unknown option"
+  exit
+fi
 scp -i doc/magoaws.pem scripts/*  core@${address}:
 scp -i doc/magoaws.pem -r conf core@${address}:
 ssh -i doc/magoaws.pem core@${address} "cp ${env_name}.env carnation.env 2>&1"
