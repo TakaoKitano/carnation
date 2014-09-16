03845019db3e43760032ffced4ce4eacd6a826bd
Takao Kitano <tkitano@gmail.com>
2014-09-16 20:39:51 +0900

    removed cadvisor, removed ExecStop scripts for carnation

--- scripts/config_service.sh
+++ scripts/config_service.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 echo retrieving docker image chikaku/carnation - this may take long time
 docker pull chikaku/carnation >/dev/null
+docker images | grep chikaku
+echo configure systemd services for carnation
+sudo systemctl enable /home/core/carnation.service 
+sudo systemctl daemon-reload
+sudo systemctl start carnation.service
 #echo retrieving docker image google/cadvisor for memory trace - this may take long time
 #docker pull google/cadvisor >/dev/null
-docker images | grep chikaku
 #docker images | grep cadvisor
-echo configure systemd services
-sudo systemctl enable /home/core/carnation.service 
 #sudo systemctl enable /home/core/cadvisor.service
-sudo systemctl daemon-reload
-sudo systemctl stop carnation.service
 #sudo systemctl stop cadvisor.service
-sudo systemctl start carnation.service
 #sudo systemctl start cadvisor.service
 sleep 5
 docker ps
