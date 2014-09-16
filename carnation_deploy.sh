412782fc9dccf9c99027a9403ac69e7c372e697f
Takao Kitano <tkitano@gmail.com>
2014-09-17 00:22:45 +0900

    added development in deploy, changed nginx upstream from localhost to 127.0.0.1 (to prevent to resolve IPv6)

--- carnation_deploy.sh
+++ carnation_deploy.sh
@@ -4,7 +4,7 @@
 #
 if [ $# -lt 2 ]
 then
-   echo "deploy {test|production} server_address"
+   echo "deploy {production|test|development} server_address"
    exit
 fi
 env_name=$1
@@ -17,11 +17,14 @@ then
 elif [[ $env_name = 'test' ]]
 then
   echo "config carnation server as test server"
+elif [[ $env_name = 'development' ]]
+then
+  echo "config carnation server as development server"
 else
   echo "unknown option"
   exit
 fi
 scp -i doc/magoaws.pem scripts/*  core@${address}:
 scp -i doc/magoaws.pem -r conf core@${address}:
-ssh -i doc/magoaws.pem core@${address} "cp ${env_name}.env carnation.env 2>&1"
+ssh -i doc/magoaws.pem core@${address} "cp ${env_name}.env carnation.env"
 ssh -i doc/magoaws.pem core@${address} "./config_service.sh"
