53c6f81d965a2f55260104b8d73234a4a4dc0414
Takao Kitano <tkitano@gmail.com>
2014-10-01 14:39:08 +0900

    added index for shot_at

--- migrate/005_timezone_shot_at.rb
+++ migrate/005_timezone_shot_at.rb
@@ -2,12 +2,14 @@ Sequel.migration do
   up do
     add_column :items, :timezone, Integer
     add_column :items, :shot_at, Integer
+    add_index  :items, :shot_at
     add_column :users, :timezone, Integer
     add_column :viewers, :timezone, Integer
   end
 
   down do
     drop_column :items, :timezone
+    drop_index  :items, :shot_at
     drop_column :items, :shot_at
     drop_column :users, :timezone
     drop_column :viewers, :timezone
